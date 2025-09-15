5a0d51b02a36b08a206932cd5df32bc567614478
Santiago Martinez <martinezortiz.santiago@gmail.com>
2025-09-15 19:27:46 +0200

    actualizar porfolio

---
+++ Santiago_Martinez_Ortiz/porfolio/src/app/app.ts
@@ -0,0 +1,21 @@
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CabeceraComponent } from './components/cabecera/cabecera';
+import { MainComponent } from './pages/main/main';
+import { FooterComponent } from './components/footer/footer';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+  CabeceraComponent,
+  MainComponent,
+  FooterComponent
+  ],
+  templateUrl: './app.html',
+  styleUrls: ['./app.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class App {
+  protected readonly title = signal('porfolio');
+}
---
+++ Santiago_Martinez_Ortiz/porfolio/src/app/pages/habilidades/habilidades.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'formacionyexperiencia-root',
+  templateUrl: './habilidades.html',
+  styleUrls: ['./habilidades.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true
+})
+export class FormacionYExperienciaComponent {}
