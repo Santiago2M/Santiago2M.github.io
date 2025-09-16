4b8c2bd3036df3bd07e27cb3953fd643a2302659
Santiago Martinez <martinezortiz.santiago@gmail.com>
2025-09-16 10:14:06 +0200

    aztualizar

--- Santiago_Martinez_Ortiz/porfolio/src/app/models/proyecto.model.ts
+++
@@ -1,7 +0,0 @@
-export interface Proyecto {
-  nombre: string;
-  descripcion: string;
-  tecnologias: string[];
-  fotoUrl: string;
-  repoUrl: string;
-}
--- Santiago_Martinez_Ortiz/porfolio/src/app/pages/porfolio/porfolio.spec.ts
+++
@@ -1,7 +0,0 @@
-import { PorfolioComponent } from './porfolio';
-describe('PorfolioComponent', () => {
-  it('should create', () => {
-    const component = new PorfolioComponent();
-    expect(component).toBeTruthy();
-  });
-});
